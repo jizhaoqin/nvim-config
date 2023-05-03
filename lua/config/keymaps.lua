802cca14770f4faa694d1bbb1656e93b6bcb8403
jizhaoqin <jizhao.qin.de@gmail.com>
2023-05-03 17:55:23 +0200

    add some keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,27 @@
 -- Add any additional keymaps here
 --
 local map = vim.keymap.set
+local opt = { noremap = true, silent = true }
 
+-- 插入模式
 map("i", "jk", "<ESC>", { noremap = true, silent = true })
+
+-- 终端模式
+map("t", "<ESC>", "<C-\\><C-n>", opt) -- back to normal mode in terminal mode
+
+-- 正常模式
 map("n", "<leader>sx", require("telescope.builtin").resume, { noremap = true, silent = true, desc = "Resume" })
+map("n", "<leader>wa", ":wall<CR>", { noremap = true, silent = true, desc = "save all modified files-jizhao" })
+-- 浏览代码
+map("n", "<C-u>", "15k", opt)
+map("n", "<C-d>", "15j", opt)
+-- 使用 alt + hjkl 在buffer之间跳转
+map("n", "<A-l>", ":bnext<CR>", opt)
+map("n", "<A-h>", ":bprevious<CR>", opt)
+-- 背景透明快捷键
+-- map(
+--   "n",
+--   "<leader>t",
+--   ":hi Normal guibg=NONE ctermbg=NONE<CR>:hi EndOfBuffer guibg=NONE ctermbg=NONE<CR>",
+--   { noremap = true, silent = true, desc = "transparent background-jizhao" }
+-- )
