c4148f2a49a7d7b6044980dfd58cd9154a4ee87f
jizhaoqin <jizhao.qin.de@gmail.com>
2023-05-03 17:55:59 +0200

    other UI settings of neovim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,10 +3,14 @@
 -- Add any additional options here
 
 -- show the up right path bar
-vim.opt.winbar = "%=%m %f"
+-- vim.opt.winbar = "%=%m %f"
+vim.opt.winbar = " "
 
 -- 右侧参考线，超过表示代码太长了，考虑换行
 vim.wo.colorcolumn = "100"
+
+-- 行结尾可以跳到下一行
+vim.o.whichwrap = "h,l"
 
 -- 两个空格等于一个tab
 vim.o.tabstop = 2
@@ -24,5 +28,8 @@ vim.o.cmdheight = 0
 vim.o.list = true
 vim.o.listchars = "space:·"
 
--- 设置主题
--- vim.cmd([[colorscheme default]])
+-- 搜索大小写不敏感，除非包含大写
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.o.hlsearch = true -- 搜索不要高亮
+vim.o.incsearch = true -- 边输入边搜索
